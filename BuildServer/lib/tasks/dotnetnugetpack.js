7f79be8c08fd447c194558527341fe2f3c7615bd
Inga Lovinde <52715130+inga-lovinde@users.noreply.github.com>
2013-12-10 14:22:50 +0400

    Implemented nuget pack task

---
+++ BuildServer/lib/tasks/dotnetnugetpack.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var dotNetBuilderWrapper = require('./dotnetbuilderwrapper');
+
+module.exports = function (params, processor) {
+	var date = new Date();
+	return dotNetBuilderWrapper({
+		command: "nugetpack",
+		BaseDirectory: processor.context.exported,
+		SpecPath: processor.context.exported + "/" + params.nuspec,
+		OutputDirectory: processor.context.release,
+		Version: (params.major || "0") + "." + date.getFullYear() + "." + ((date.getMonth() + 1) * 100 + date.getDay()) + "." + ((date.getHours() * 100 + date.getMinutes()) * 100 + date.getSeconds())
+	}, processor);
+};
