4c363f9e0a8828dfbfcc8581a092a3e889c0da20
Inga Lovinde <52715130+inga-lovinde@users.noreply.github.com>
2015-10-22 13:55:21 +0300

    Fixed dotnetnugetpushonly

--- BuildServer/lib/tasks/dotnetnugetpush.js
+++ BuildServer/lib/tasks/dotnetnugetpush.js
@@ -6,7 +6,7 @@ var settings = require("../../settings");
 module.exports = function (params, processor) {
 	var date = new Date(),
 		version = (params.version || ((params.major || "0") + "." + (date.getFullYear() * 10000 + (date.getMonth() + 1) * 100 + date.getDate()) + "." + ((date.getHours() * 100 + date.getMinutes()) * 100 + date.getSeconds()))) + "-r" + processor.context.rev.substr(0, 16),
-		nupkg = processor.context.exported + "/" + params.name + "." + version + ".nupkg";
+		nupkg = params.name + "." + version + ".nupkg";
 
 	return sequential({
 		tasks: [
--- BuildServer/lib/tasks/dotnetnugetpushonly.js
+++ BuildServer/lib/tasks/dotnetnugetpushonly.js
@@ -1,13 +1,13 @@
 "use strict";
 
 var sequential = require("./sequential");
-var dotnetbuilderwrapper = require('dotnetbuilderwrapper');
+var dotnetbuilderwrapper = require('./dotnetbuilderwrapper');
 var settings = require("../../settings");
 
 module.exports = function (params, processor) {
 	return dotnetbuilderwrapper({
 		command: "nugetpush",
-		Package: params.Package,
+		Package: processor.context.exported + "/" + params.Package,
 		NugetHost: settings.nugetHost,
 		ApiKey: settings.nugetApiKey
 	}, processor);
