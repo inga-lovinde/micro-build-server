5fa67242152887f69c86187132c133809c363c10
Inga Lovinde <52715130+inga-lovinde@users.noreply.github.com>
2015-10-26 11:14:56 +0300

    deletefromcode task implemented

---
+++ BuildServer/lib/tasks/deletefromcode.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var fse = require('fs-extra');
+
+module.exports = function (params, processor) {
+	return {
+		process: function () {
+			var sourceFilePath = processor.context.exported + "/" + params.filename;
+
+			processor.onInfo("Deleting " + sourceFilePath);
+
+			fse.remove(sourceFilePath, function(err) {
+				if (err) {
+					processor.onError("Unable to delete file: " + err);
+				} else {
+					processor.onInfo("Deleted file");
+				}
+				return processor.done();
+			});
+		}
+	};
+};
