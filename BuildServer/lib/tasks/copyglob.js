ba166f7d6a0a0a78491f3aa30b390d46845f1051
Inga Lovinde <52715130+inga-lovinde@users.noreply.github.com>
2016-03-10 08:51:36 +0300

    Copyglob task implemented

---
+++ BuildServer/lib/tasks/copyglob.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var fse = require('fs-extra');
+var glob = require('glob');
+
+module.exports = function (params, processor) {
+	return {
+		process: function () {
+			glob(params.mask, {
+				dot: true,
+				cwd: processor.context.exported
+			}, function (err, files) {
+				if (err) {
+					processor.onError(err);
+					return processor.done();
+				}
+
+				if (!files || !files.length) {
+					return processor.done();
+				}
+
+				return processor.processTask({
+					type: "parallel",
+					params: {
+						tasks: files.map(function (file) {
+							return {
+								name: file,
+								type: "copy",
+								params: {
+									filename: file
+								}
+							};
+						})
+					}
+				}, processor.done.bind(processor));
+			});
+		}
+	};
+};
